5b6f5c8255df1b9ede4b581fc3483ad78e0ef7bb
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-10-04 17:58:45 +0400

    create a simple calculater in quiz component to showcase reactivity in svelte

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,10 +1,16 @@
 <script>
   export let quizName = "Svelte Quiz";
-  let title = "";
+  // let title = "";
+  let a = 0;
+  let b = 0;
 </script>
 
 <div>
   <h2>{quizName}</h2>
-  <h4>{title}</h4>
-  <input bind:value={title} type="text" >
+  <!-- <h4>{title}</h4> -->
+  <!-- <input bind:value={title} type="text" > -->
+  <h3>Calculate:</h3>
+  <input bind:value={a} type="number">
+  <input bind:value={b} type="number">
+  <p>The sum is {a + b}</p>
 </div>
