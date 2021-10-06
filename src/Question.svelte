a46550f9d15d97a92c56d37e518fb49dc01eadfa
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-10-06 09:37:11 +0400

    add answer buttons, shuffled, with check for crrect answer, all in quiz component

--- src/Question.svelte
+++ src/Question.svelte
@@ -1,15 +1,52 @@
 <script>
   export let question;
 
-  let answers = question.incorrect_answers;
+  let isCorrect;
+  let isAnswered = false;
+
+  let wrongAnswers = question.incorrect_answers.map(answer => {
+    return {
+      answer,
+      correct: false
+    }
+  });
+
+  let answers = [
+    ...wrongAnswers,
+    {
+      answer: question.correct_answer,
+      correct: true
+    }
+  ]
+
+  const shuffle = (arr) => {
+    arr.sort(() => Math.random() - 0.5)
+  }
+
+  shuffle(answers);
+
+  const checkAnswer = (correct) => {
+    isAnswered = true;
+    isCorrect = correct;
+  }
 </script>
 
 
 
 <h3>{@html question.question}</h3>
 
+{#if isAnswered}
+  <h4>
+    {#if isCorrect}
+      You got it right!
+    {:else}
+      Wrong answer!
+    {/if}
+  </h4>
+{/if}
+
 {#each answers as answer}
-  <button>
-    {@html answer}
+  <button on:click={() => checkAnswer(answer.correct)}>
+    {@html answer.answer}
   </button>
 {/each}
