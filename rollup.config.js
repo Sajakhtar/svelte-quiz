3f86991d8138de24e4f654da545700d2b7151a3c
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-10-07 22:14:18 +0400

    install svelte preprocessor, add svelte config js, add scss preprocessor

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
+import autoPreprocess from 'svelte-preprocess';
+import sveltePreprocess from 'svelte-preprocess';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
@@ -38,6 +40,7 @@ export default {
 	},
 	plugins: [
 		svelte({
+      preprocess: autoPreprocess(),
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
